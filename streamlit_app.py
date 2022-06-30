6450777109ed0f7b6ecf68e37ad739fa9329f6d0
Mensadata <108447894+Mensadata@users.noreply.github.com>
2022-06-30 09:16:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents New Healthy Diner!')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣🥣Omega 3 & Blue Berry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard Boiled free range egg')
+streamlit.header('🍎🍊Build your own fruite smoothie🥭🍓')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
