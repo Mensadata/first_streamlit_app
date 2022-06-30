1532f65797364e2ab8587dd6c676166ae6e4d9e0
Mensadata <108447894+Mensadata@users.noreply.github.com>
2022-06-30 00:12:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title("Welcome to the Streamlit world")
 streamlit.header("My name is Venkat!")
-streamlit.text("Started learning streamlit")
+streamlit.text("Started learning streamlit &#x1F923;")
