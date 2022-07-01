648034b0fde6aaba3184c04dce50039a82568f26
Mensadata <108447894+Mensadata@users.noreply.github.com>
2022-07-01 15:03:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-request = "https://fruityvice.com/api/fruit/" + fruit
+request = "https://fruityvice.com/api/fruit/" + fruit_choice
 fruityvice_response = requests.get(request)
 
 # take the json text from api and load to pandas to normaize it 
