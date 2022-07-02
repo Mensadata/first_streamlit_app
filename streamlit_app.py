e47cd4e48100490ad4ad82f0737e732636e89e25
Mensadata <108447894+Mensadata@users.noreply.github.com>
2022-07-02 11:15:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUITYVICE")
+my_cur.execute("SELECT FAMILY, NAME FROM FRUITYVICE")
 my_data_row = my_cur.fetchone()
 streamlit.header("The frutvice lisr contains:")
 streamlit.text(my_data_row)
